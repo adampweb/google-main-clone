1e2e1eaa26fdeb1398c1ebb84355460ff0e7f85b
adampweb <adamweb92@gmail.com>
2023-04-25 09:11:17 +0200

    Added disclaimer message

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/header";
 import MainTab from "./components/main/tabs/MainTab";
 import ImagesTab from "./components/main/tabs/ImagesTab";
@@ -7,6 +7,18 @@ import Footer from "./components/footer";
 const App = () => {
     const [isShownMainTab, setIsShownMainTab] = useState(true);
     const [isShownImagesTab, setIsShownImagesTab] = useState(false);
+
+    function usePersistedState(key: string, defaultValue: string) {
+        const [state, setState] = useState(
+            localStorage.getItem(key) || defaultValue
+        );
+        useEffect(() => {
+            localStorage.setItem(key, state);
+        }, [key, state]);
+        return [state, setState];
+    }
+    const [isShownMessage, setIsShownMessage] = useState(localStorage.getItem("showWarning") || "true");
+
 
     const showMainTab = (event: any) => {
         // 👇️ toggle shown state
@@ -20,11 +32,24 @@ const App = () => {
         setIsShownMainTab(false);
     };
 
+    const hideMessage = (event: any) => {
+        localStorage.setItem("showWarning", "false");
+        setIsShownMessage('false');
+        console.log(isShownMessage)
+    }
+
     return (
         <div className="App" id={"gb-main"}>
             <Header mainTabFn={{showMainTab}} imgTabFn={{showImagesTab}} tabStates={{mainTabState: isShownMainTab, imgTabState: isShownImagesTab}}/>
             {isShownMainTab && <MainTab style={{display: isShownMainTab ? 'block' : 'none' }}/>}
             {isShownImagesTab && <ImagesTab style={{display: isShownImagesTab ? 'block' : 'none' }}/>}
+            {isShownMessage === "true" &&
+                <div id="disclaimer" style={{display: isShownMessage === "true" ? 'block' : 'none' }}>
+                    <h2><span>⚠️</span><span>⚠️</span><span>⚠️</span><br/>This clone app created only for learning purpose!</h2>
+                    <button type={"button"} id={"hideWarningMsg"} onClick={(e) => hideMessage(e)}>Accept & hide message</button>
+                </div>
+            }
+
             <Footer/>
         </div>
     );
